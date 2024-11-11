5a9c901a2697a673842c604983939286469e9026
MatheusNavarro-5159125 <matheusranzatti@edu.uniube.br>
2024-11-11 18:07:34 -0300

    front-end

---
+++ front/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+    { path: '', component: AppComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
